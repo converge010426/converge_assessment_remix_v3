bc22e484e4d31c82d14cab473c42866937b902a7
CONVERGE Assessments <tomknsn@gmail.com>
2026-09-16 06:35:32 +0200

    Activate controlled V2 market fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import { installV2MarketFixes } from './v2MarketFixes';
 import './index.css';
+
+installV2MarketFixes();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
