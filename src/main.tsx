13a98828d87cddcf25f65898d22a2ef155e0601d
CONVERGE Assessments <tomknsn@gmail.com>
2026-09-22 09:52:43 +0200

    Remove legacy navigation and hero DOM workarounds

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import { installV2MarketFixes } from './v2MarketFixes';
-import { installHeroFinalFix } from './heroFinalFix';
-import { installNavigationRepeatFix } from './navigationRepeatFix';
 import './index.css';
 
 installV2MarketFixes();
-installNavigationRepeatFix();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -17,5 +14,3 @@ createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </StrictMode>,
 );
-
-window.setTimeout(installHeroFinalFix, 350);
